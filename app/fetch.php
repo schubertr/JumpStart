d85138a21cef718b73c25a095bd74935afca161a
s-chube <schubertr@etown.edu>
2016-03-22 23:12:04 -0400

    prompts for file download based on selections

--- app/fetch.php
+++ app/fetch.php
@@ -20,11 +20,39 @@ input.hide{visibility: hidden}
 <?php
 
 $choices = $_GET;
+$i = 0;
+
+$myfile = fopen("testFile.txt", "w") or die("Unable to open file!");
 	
-	foreach ($choices as $index=>$val) 
-	{
-		print "$index";
-	}
+foreach ($choices as $index=>$val) 
+{
+	fwrite($myfile, $index);
+	fwrite($myfile, "\n");
+}
+
+fclose($myfile);
+
+print "<prev>";
+print_r($choices);
+print "</prev>";
+
+$file = 'testFile.txt';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
+}
+
 ?>
 </body>
 
