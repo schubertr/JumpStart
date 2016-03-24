c9c4ad35929278358876deec0c71c22bbc2da2de
s-chube <schubertr@etown.edu>
2016-03-24 17:45:12 -0400

    began working on scripts

---
+++ scripts/base.sh
@@ -0,0 +1,15 @@
+#Base Script
+
+if [ "$USER" != "root" ]
+then
+	echo "Please run this script again as sudo"
+	exit 1
+fi
+
+HASBREW=$(which brew)
+
+if [ -z "$HASBREW" ]
+then
+	echo Please install Homebrew package manager.
+	exit 1
+fi
